f8287a6ce48e045b6049e8e86e94ca875ddec8ff
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2023-12-19 21:16:35 -0500

    Update build.sh

--- mainline/build.sh
+++ mainline/build.sh
@@ -37,7 +37,7 @@ sudo ln -sf aarch64-linux-gnu-gcov-tool-12 /usr/bin/aarch64-linux-gnu-gcov-tool
 git clone --branch master "https://github.com/rockchip-linux/rkbin.git" rkbin
 
 git clone --branch ${ubootRef} "${ubootRepo}" u-boot
-
+echo -e "CONFIG_ROCKCHIP_SPI_IMAGE=y" >> $rootdir/u-boot/configs/${boardconfig}
 mkdir $rootdir/out
 
 export ROCKCHIP_TPL=$rootdir/rkbin/$(confget -f $rootdir/rkbin/RKBOOT/RK3588MINIALL.ini -s LOADER_OPTION FlashData)
