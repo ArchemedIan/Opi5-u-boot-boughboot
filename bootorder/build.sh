40a5d56dc8f161b641c4cf21d0546c1e1d23fbfa
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2024-01-14 08:33:21 -0500

    Update build.sh

--- bootorder/build.sh
+++ bootorder/build.sh
@@ -44,7 +44,7 @@ git clone --branch ${ubootRef} "${ubootRepo}" u-boot
 grep "CONFIG_ROCKCHIP_SPI_IMAGE=y" $rootdir/u-boot/configs/${boardconfig} >/dev/null || echo -e "CONFIG_ROCKCHIP_SPI_IMAGE=y" >> $rootdir/u-boot/configs/${boardconfig}
 echo -e "CONFIG_USE_PREBOOT=y" >> $rootdir/u-boot/configs/${boardconfig}
 echo -e "CONFIG_PREBOOT=\"setenv boot_targets \\\"${order}\\\"\"" >> $rootdir/u-boot/configs/${boardconfig}
-echo -e "CONFIG_BOOTCOMMAND=\"pci enum; nvme scan; bootflow scan\"" >> $rootdir/u-boot/configs/${boardconfig}
+echo -e "CONFIG_BOOTCOMMAND=\"bootflow scan -b\"" >> $rootdir/u-boot/configs/${boardconfig} #pci enum; nvme scan;
 echo -e "CONFIG_BOOTSTD_FULL=y" >> $rootdir/u-boot/configs/${boardconfig}
 
 cp $rootdir/v2-1-4-rockchip-rk3588-Fix-boot-from-SPI-flash.diff $rootdir/u-boot/
